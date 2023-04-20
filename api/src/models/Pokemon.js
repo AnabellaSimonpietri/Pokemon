67d4d78afa21d668d47bb3d428d162b84065cc4d
AnabellaSimonpietri <simonpietrianabella@gmail.com>
2023-04-20 02:00:29 -0300

    falta rutas types

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -1,12 +1,56 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      img: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      life: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      attack: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      defense: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      speed: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      createBD: {
+        //Todo lo que yo cree se crea con esta propiedad, es más eficiente.
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
-  });
+    { timestamps: true } // ¿o false? //
+  );
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,76 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require("axios");
+const { Pokemon, Type } = require("../db");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+// Esta función trae la info de la api:
+const getApiInfo = async () => {
+  const {
+    data: { results },
+  } = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=60");
+  const apiInfo = await Promise.all(
+    results.map(async (result) => {
+      const { data } = await axios.get(result.url);
+      return {
+        id: data.id,
+        name: data.name,
+        img: data.sprites.other.home.front_default,
+        life: data.stats[0].base_stat,
+        attack: data.stats[1].base_stat,
+        defense: data.stats[2].base_stat,
+        speed: data.stats[5].base_stat,
+        height: data.height,
+        weight: data.weight,
+        types: data.types.map((type) => type.type.name),
+      };
+    })
+  );
+
+  return apiInfo;
+};
+
+// Esta función trae la data de la Base de Datos:
+const getDbInfo = async () => {
+  return await Pokemon.findAll({
+    // Traeme toda la info
+    include: {
+      model: Type, // Y traeme los tipos y los nombres
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+};
+
+const getAllPokemons = async () => {
+  // Unifico los Pokemons de mi DB y mi API
+  const apiInfo = await getApiInfo();
+  const dbInfo = await getDbInfo();
+  const infoTotal = apiInfo.concat(dbInfo);
+  return infoTotal;
+};
+
+router.get("/pokemons", async (req, res) => {
+  // ¿pokemonS o pokemoN?
+  const name = req.query.name; // Busca nombre por Query
+  let pokemonsTotal = await getAllPokemons();
+  if (name) {
+    let pokemonName = await pokemonsTotal.filter((el) =>
+      el.name.toLowerCase().includes(name.toLowerCase())
+    ); // Busqueda mínuscula y mayúscula
+    pokemonName.length
+      ? res.status(200).send(pokemonName)
+      : res.status(404).send("Sorry, that Pokemon does not exist"); // Pokemon no existe
+  } else {
+    res.status(200).send(pokemonsTotal); // Todos
+  }
+});
 
 module.exports = router;
